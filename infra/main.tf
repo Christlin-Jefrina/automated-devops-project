a7c3429762032361e52bbbbc56440262f55de26a
Christlin-Jefrina <christlinjefrina@karunya.edu.in>
2025-10-30 01:35:14 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -58,3 +58,10 @@ resource "random_string" "suffix" {
   upper   = false
   numeric = true
 }
+
+# Add this resource block to grant AKS pull rights to ACR
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
